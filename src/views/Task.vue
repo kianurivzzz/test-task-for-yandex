015979728b5c7034bb0637c8385a1a29c615c6dd
Nikita Karasyov <nikitakarasev177@gmail.com>
2023-09-18 23:19:51 +0300

    fix Task.vue

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Тут надо задачу для детей</h1>
-
+    <div style="margin-top: 2em;">
+        <h3 class="font-semibold" style="font-size: 1.5rem; line-height: 2rem;">Интересная задача</h3>
+        <p class="text-gray-600 mt-2">Создайте функцию, которая принимает целое число в качестве
+            аргумента и возвращает результат "Even"для четных или
+            "Odd"нечетных чисел. То есть, заставьте компьютер понимать где чётное, а где нечётное число</p>
+    </div>
+    <div style="margin-top: 2em;">
+        <h3 class="font-semibold mt-10" style="font-size: 1.5rem; line-height: 2rem;">Почему именно она?</h3>
+        <p class="text-gray-600 mt-2">Смысл скорее не в задаче, а в её подаче. Этой задачей можно показать базовые основы
+            программирования и показать,
+            что компьютером можно управлять.</p>
     </div>
 </template>
